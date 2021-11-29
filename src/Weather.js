03146b77cff3e04d22051d62265f492079decd3e
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2021-11-29 08:32:09 +0100

    first commitment

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props){
+ function handleResponse(response){
+    alert(
+     `The weather in Tokyo is 
+      ${response.data.main.temp}°C`
+     );
+    }
+    let apiKey= "42ef142def142e37f3591de51015042b";
+    let unit= "metric";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${unit}`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return <h2>Hello from react</h2>;
+}
