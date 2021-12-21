84e268557b759d8b9d8c1e098db47ec08ba3b9a5
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2021-12-22 00:46:18 +0900

    Removed ./Weather from index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 import App from "./App";
-import Weather from "./Weather";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Weather />
     <App />
     </StrictMode>,
   rootElement
