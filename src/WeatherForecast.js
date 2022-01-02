3693788142ef0c21c2c990e578ea9831596e5fdb
Marin.R <88306713+mariiiiin913@users.noreply.github.com>
2022-01-02 18:15:15 +0900

    Adjusted WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./WeatherForecast.css"
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -17,31 +17,37 @@ export default function WeatherForecast(props){
         setLoaded(true);
     }
 
-    if (loaded){
-      return(
-        <div className="weather-forecast">
-                <div className="row">
-                  {forecast.map(function(dailyforecast, index){
-                    if(index < 6){
-                    return(
-                    <div className="col-2" key={index}>
-                    <WeatherForecastDay data={dailyforecast}/>
-                 </div>
-                    );
-                    }
-                  })}      
-        </div>
-      </div>
-      );
-
-    }else{
-    
+    function load(){
       let apiKey="93032777a0ea3ff06e3d56ac2a07c202";
       let longitude= props.coordinates.lon;
       let latitude= props.coordinates.lat;
       let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   
       axios.get(apiUrl).then(handleResponse);
+
+    }
+
+    if (loaded){
+      return(
+        <div className="weather-forecast">
+                <div className="row">
+                  {forecast.map(function(dailyForecast, index) {
+                    if(index < 6){
+                    return(
+                    <div className="col" key={index} >
+                    <WeatherForecastDay data={dailyForecast}/>
+                    </div>
+                    );
+                    }else{
+                      return null;
+                    }
+                  })}     
+             </div>
+         </div>
+      );
+    }else{
+         
+      load();
   
       return null;
   }
